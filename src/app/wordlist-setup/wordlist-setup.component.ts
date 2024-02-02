f24d16483204bf8e84a34f01b1e6a4f82c6ac60e
clementbrandenbourger <brandenbourger.c@sfeir.com>
2024-02-02 12:22:41 +0100

    Setup interface placeholder

--- src/app/wordlist-setup/wordlist-setup.component.ts
+++ src/app/wordlist-setup/wordlist-setup.component.ts
@@ -1,9 +1,21 @@
 import { Component } from '@angular/core';
+import {MatIcon} from "@angular/material/icon";
+import {MatButton, MatFabButton} from "@angular/material/button";
+import {MatCard, MatCardActions, MatCardHeader, MatCardSubtitle, MatCardTitle} from "@angular/material/card";
 
 @Component({
   selector: 'app-wordlist-setup',
   standalone: true,
-  imports: [],
+  imports: [
+    MatIcon,
+    MatFabButton,
+    MatCard,
+    MatCardHeader,
+    MatCardActions,
+    MatButton,
+    MatCardSubtitle,
+    MatCardTitle
+  ],
   templateUrl: './wordlist-setup.component.html',
   styleUrl: './wordlist-setup.component.css'
 })
