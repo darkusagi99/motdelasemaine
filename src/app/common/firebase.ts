0a00c27fca0a7bad7e85c05f1db748ec8286941a
clementbrandenbourger <brandenbourger.c@sfeir.com>
2024-02-07 16:24:38 +0100

    With firebase file for GitHub actions

---
+++ src/app/common/firebase.ts
@@ -0,0 +1,13 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth"
+import {NgModule} from "@angular/core";
+
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = ##FIREBASE_CONFIG##
+
+export default firebaseConfig;
